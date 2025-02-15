d78b3dff9e75cd98ac6401d7262c8bfd5e95bbef
nuraalhaji <danamonuraa@gmail.com>
2025-02-15 12:38:38 +0100

    dna

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const express = require("express");
+const port = process.env.port || 3000
+require("dotenv").config()
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const  upload = require("./cloudinary/upload");
+
+
+
+const app = express()
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+const corsConfig = {
+    origin :["*"],
+    credential : true,
+    methods : ["GET","POST","PUT","DELETE"]
+}
+
+app.use(cors(corsConfig))
+
+app.use("/file",express.static("cloud/"))
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+app.post("/post",upload.single("file"),(req,res)=>{
+    console.log(req.file)
+    
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log("server is listening")
+})
