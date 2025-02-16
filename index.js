3f2b8a93d43c3e089fb39e28be1a1a0542a654d7
nuraalhaji <danamonuraa@gmail.com>
2025-02-16 21:27:37 +0100

    commit

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 
-const express = require("express");
-const port = process.env.port || 3000
-require("dotenv").config()
-const cors = require("cors");
-const  upload = require("./cloudinary/upload");
-
-
-
+const express  = require("express");
+const Port = process.env.PORT || 3000
 const app = express()
+const cors = require("cors");
+const multer = require("multer");
+
+
 const corsConfig = {
     origin : ["https://fastapi-iota-lac.vercel.app"],
     credential : true,
@@ -18,19 +16,27 @@ app.options("",cors(corsConfig))
 app.use(cors(corsConfig))
 app.use(express.json())
 
-app.use("/file",express.static("cloud/"))
+const storage = multer.diskStorage({ destination : "uploads/image",
+    filename:(req, file, cb)=> {
+cb(null, `${file.originalname}`)
+}})
+app.use("/file", express.static("uploads/image"))
+const upload = multer({storage:storage})
+
+
+
 app.get("/",(req,res)=>{
-    res.send("hello world")
+    res.send("hello from backend")
 })
 
-app.post("/post",upload.single("file"),(req,res)=>{
-    console.log(req.file)
+
+app.post('/api/users',upload.single("file"),(req,res) => {
     
+                    
 })
 
 
-
-
-app.listen(port,()=>{
-    console.log("server is listening")
+app.listen(Port, ()=>{
+    console.log("server is Running")
+    
 })
